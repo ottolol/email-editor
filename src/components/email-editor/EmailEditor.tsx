1fb3fd0b9fa2e0fd4c50fd3d85ded024f1f149ba
ottolol <alexsveshnik@gmail.com>
2024-03-10 18:35:01 +0300

    html and css done

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,14 +1,28 @@
+import { Bold, Italic, Pencil, Underline } from "lucide-react";
 import styles from "./EmailEditor.module.css";
 
 export default function EmailEditor() {
   return (
     <div className={styles.emailEditor}>
-      <h2>Email-Editor</h2>
-      <div>
+      <div className={styles.emailEditor__title}>
+        <h2>Email-Editor</h2>
+      </div>
+      <div className={styles.emailEditor__text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt minima ex
         eaque blanditiis odio tenetur, voluptas velit exercitationem quas
         labore, repellat assumenda provident suscipit quis. Quos, cupiditate?
         Laborum, reprehenderit accusantium.
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footer__icons}>
+          <Pencil />
+          <Bold />
+          <Italic />
+          <Underline />
+        </div>
+        <div className={styles.footer__button}>
+          <button>Send Now!</button>
+        </div>
       </div>
     </div>
   );
